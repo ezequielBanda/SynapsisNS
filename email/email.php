2a2558265d7962b5f7c1fb5564a51524c51a1089
ezequielBanda <ezequiel.bandap@gmail.com>
2016-11-05 15:51:30 -0700

    clase mail
    listo checalo si me funciono

--- email/email.php
+++ email/email.php
@@ -1,34 +1,86 @@
 <?php
-include_once('PHPMailer/class.phpmailer.php');
-
-class email{
+if(isset($_POST['mail'])) {
+     // 
+    $email_to = "ezequiel.bandap@gmail.com";
+    $email_subject = "PRUEBA";
     
-    function enviarEmail(){
-// Instancia de la clase PHPMailer
-	$mail = new PHPMailer(); 
-
-        $email=$emailEnv; // Recipients email ID  
-        $name="Administracion SynapsisPharma"; // Recipient's name  
-        $mail->From = "ezequiel.bandap@gmail.com";  
-        $mail->FromName = "lo que sea";  
-        $mail->AddAddress($email);  
-        $mail->AddReplyTo("No-Reply");  
-        $mail->WordWrap = 50; // set word wrap  
-        $mail->IsHTML(true); // send as HTML  
-        $mail->Subject = "lo que sea";  
-        $mail->Body = "";//HTML Body  
-        $mail->AltBody = "This is the body when user views in plain text format"; //Text Body  
-        if(!$mail->Send())  
-        {  
-          echo "Mailer Error: " . $mail->ErrorInfo;  
-        }  
-        else  
-        {  
-        //	header("Location:../login/envio.php");
-        }
-    
-    
+    function died($error) {
+ 
+        // mensajes de error
+        echo "Lo sentimos, hubo un error en sus datos y el formulario no puede ser enviado en este momento. ";
+        echo "Detalle de los errores.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Porfavor corrija estos errores e inténtelo de nuevo.<br /><br />";
+        die();
     }
-    
+ 
+    // Se valida que los campos del formulairo estén llenos
+    if(!isset($_POST['name']) ||
+       !isset($_POST['phone']) ||
+       !isset($_POST['lastname']) ||
+       !isset($_POST['mail'])
+       ||!isset($_POST['message'])) {
+        die('Lo sentimos pero parece haber un problema con los datos enviados.');
+    }
+    //En esta parte el valor "name"  sirve para crear las variables que recolectaran la información de cada campo
+    $name = $_POST['name']; // requerido
+    $phone = $_POST['phone']; // requerido
+    $lastname = $_POST['lastname']; // requerido
+    $email_from = $_POST['mail']; // no requerido 
+    $message = $_POST['message']; // requerido
+    $error_message = "";//Linea numero 52;
+ 
+//En esta parte se verifica que la dirección de correo sea válida 
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+    if(!preg_match($email_exp,$email_from)) {
+        $error_message .= 'La dirección de correo proporcionada no es válida.<br />';
+    }
+ 
+//En esta parte se validan las cadenas de texto
+    $string_exp = "/^[A-Za-z .'-]+$/";
+    if(!preg_match($string_exp,$name)) {
+        $error_message .= 'El formato del nombre no es válido<br />';
+    }
+ 
+  if(!preg_match($string_exp,$lastname)) {
+      $error_message .= 'el formato del apellido no es válido.<br />';
+  }
+ 
+  if(strlen($message) < 2) {
+      $error_message .= 'El formato del texto no es válido.<br />';
+  }
+ 
+  if(strlen($error_message) > 0) {
+      die($error_message);
+  }
+ 
+//Este es el cuerpo del mensaje tal y como llegará al correo
+ 
+    $email_message = "Contenido del Mensaje.\n\n";
+    function clean_string($string) {
+        $bad = array("content-type","bcc:","to:","cc:","href");
+        return str_replace($bad,"",$string);
+    }
+    $email_message .= "Nombre: ".clean_string($name)."\n";
+    $email_message .= "Telefono: ".clean_string($phone)."\n";
+    $email_message .= "Asunto: ".clean_string($lastname)."\n";
+    $email_message .= "Correo: ".clean_string($mail)."\n";
+    $email_message .= "Mensaje: ".clean_string($message)."\n";
+    //Se crean los encabezados del correo
+    $headers = 'From: '.$email_from."\r\n".
+        'Reply-To: '.$email_from."\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);
+?>
+ 
+ 
+ 
+<!-- Mensaje de que fue enviado-->
+ 
+Gracias! Nos pondremos en contacto contigo a la brevedad
+ 
+<?php
+ 
 }
+ 
 ?>
